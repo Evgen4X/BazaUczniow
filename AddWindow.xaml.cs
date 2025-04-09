ac5df2511ad82725d7b8e091cf0496fcfcd9430a
Evgen4X <131477076+Evgen4X@users.noreply.github.com>
2025-04-09 12:58:24 +0200

    09.04.2025

---
+++ AddWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BazaUczniow
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        public MainWindow parent;
+        public AddWindow()
+        {
+            InitializeComponent();
+        }
+
+        private bool checkFilled()
+        {
+            bool flag = true;
+            if(i_pesel.Text.Trim().Length == 0)
+            {
+                i_pesel.Background = Brushes.Red;
+                flag = false;
+            }
+            if (i_imie.Text.Trim().Length == 0)
+            {
+                i_imie.Background = Brushes.Red;
+                flag = false;
+            }
+            if (i_nazwisko.Text.Trim().Length == 0)
+            {
+                i_nazwisko.Background = Brushes.Red;
+                flag = false;
+            }
+            if (i_data.Text.Trim().Length == 0)
+            {
+                i_data.Background = Brushes.Red;
+                flag = false;
+            }
+            if (i_adres.Text.Trim().Length == 0)
+            {
+                i_adres.Background = Brushes.Red;
+                flag = false;
+            }
+            if (i_miejscowosc.Text.Trim().Length == 0)
+            {
+                i_miejscowosc.Background = Brushes.Red;
+                flag = false;
+            }
+            if (i_kodPocztowy.Text.Trim().Length == 0)
+            {
+                i_kodPocztowy.Background = Brushes.Red;
+                flag = false;
+            }
+
+
+            return flag;
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            if (!checkFilled())
+            {
+                return;
+            }
+
+            parent.addItem(new Uczen(i_pesel.Text, i_imie.Text, i_drugieImie.Text, i_nazwisko.Text, i_data.Text, i_telefon.Text, i_adres.Text, i_miejscowosc.Text, i_kodPocztowy.Text));
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BazaUczniow
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+  
+    public class Uczen
+    {
+        public string m_PESEL;
+        public string m_imie;
+        public string m_drugieImie;
+        public string m_nazwisko;
+        public string m_data;
+        public string m_telefon;
+        public string m_adres;
+        public string m_miejscowosc;
+        public string m_kodPocztowy;
+
+        public Uczen(string m_PESEL, string m_imie, string m_drugieImie, string m_nazwisko, string  m_data, string m_telefon, string m_adres, string m_miejscowosc, string m_kodPocztowy)
+        {
+            this.m_PESEL = m_PESEL;
+            this.m_imie = m_imie;
+            this.m_drugieImie = m_drugieImie;
+            this.m_nazwisko = m_nazwisko;
+            this.m_data = m_data;
+            this.m_telefon = m_telefon;
+            this.m_adres = m_adres;
+            this.m_miejscowosc = m_miejscowosc;
+            this.m_kodPocztowy = m_kodPocztowy;
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void addItem(Uczen uczen)
+        {
+            listView.Items.Add(uczen);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddWindow w = new AddWindow();
+            w.parent = this;
+            w.Show();
+        }
+    }
+}
